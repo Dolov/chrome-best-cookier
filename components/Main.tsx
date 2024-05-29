9262bb77168aee84c8dbb950ef10acd61d63960d
石松岩 <songyan_shi@163.com>
2024-05-29 11:09:22 +0800

    结构拆分

---
+++ components/Main.tsx
@@ -0,0 +1,85 @@
+import React from 'react'
+import Actions from '~components/Actions'
+import DataList, { type DataListProps } from '~components/DataList'
+import { type Cookie, getDomainList, MessageActionEnum } from '~utils'
+
+export interface MainProps {
+  
+}
+
+const Main = props => {
+  const { urlInfo } = props
+  const { domain, subdomain } = urlInfo
+  const [cookies, setCookies] = React.useState<Cookie[]>([])
+  const [conditions, setConditions] = React.useState<DataListProps["conditions"]>(() => {
+    return {
+      name: "",
+      value: "",
+      path: "",
+      domainList: getDomainList(domain, subdomain),
+    }
+  })
+
+
+  const init = async () => {
+    const cookies = await chrome.runtime.sendMessage({
+      action: MessageActionEnum.GET_COOKIES,
+      payload: {
+        domain
+      }
+    })
+
+    const domainCookies: Cookie[] = cookies
+      .sort((a, b) => {
+        return a.name.localeCompare(b.name)
+      })
+
+    setCookies(domainCookies)
+  }
+
+  React.useEffect(() => {
+    init()
+  }, [])
+
+  const filteredCookies = React.useMemo(() => {
+    const { path, name, value, domainList } = conditions
+    if (domainList.length) {
+      return cookies.filter(item => {
+        if (item.create) return true
+        if (path && item.path) return item.path.toLowerCase().includes(path.toLowerCase())
+        if (name && item.name) return item.name.toLowerCase().includes(name.toLowerCase())
+        if (value && item.value) return item.value.toLowerCase().includes(value.toLowerCase())
+        return domainList.includes(item.domain)
+      })
+    }
+    return cookies.filter(item => {
+      if (item.create) return true
+      if (path && item.path) return item.path.toLowerCase().includes(path.toLowerCase())
+      if (name && item.name) return item.name.toLowerCase().includes(name.toLowerCase())
+      if (value && item.value) return item.value.toLowerCase().includes(value.toLowerCase())
+      return true
+    })
+  }, [cookies, conditions])
+
+  return (
+    <div className="flex flex-col w-[700px] max-h-[568px] min-h-[256px]">
+      <div className="flex-1 overflow-x-auto">
+        <DataList
+          init={init}
+          allCookies={cookies}
+          value={filteredCookies}
+          onChange={setCookies}
+          urlInfo={urlInfo}
+          conditions={conditions}
+          setConditions={setConditions}
+        />
+      </div>
+      <Actions
+        init={init}
+        cookies={filteredCookies}
+      />
+    </div>
+  )
+}
+
+export default Main
--- tabs/full.tsx
+++ tabs/full.tsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { type Cookie, MessageActionEnum, getDomainList } from '~utils'
+import '~/style.less'
+
 
 export interface AppProps {
   
